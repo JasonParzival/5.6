1336c5f233f815199900d394c4e2940e25c196e1
JasonParzival <ruslanmuhamed123@gmail.com>
2025-05-03 18:23:21 +0800

    Задание выполнено

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@
     require_once "../controllers/SearchController.php";
     require_once "../controllers/PortalObjectCreateController.php";
     require_once "../controllers/PortalObjectTypesController.php";
+    require_once "../controllers/PortalObjectDeleteController.php";
 
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader, [
@@ -27,6 +28,7 @@
     $router->add("/search", SearchController::class);
     $router->add("/create", PortalObjectCreateController::class);
     $router->add("/types", PortalObjectTypesController::class);
+    $router->add("/portal-character/(?P<id>\d+)/delete", PortalObjectDeleteController::class);
 
     $router->get_or_default(Controller404::class);
 ?>
